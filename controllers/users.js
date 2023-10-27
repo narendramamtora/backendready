91af72b98b36c6baf391f5a95ca7e3a038330336
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2023-10-27 20:49:07 +0530

    signup backend
    signupbackend creation

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,33 @@
 const Users = require('../models/users');
-exports.createUser=(req, res,next) => {
-    const Name=req.body.name;
-    const Email=req.body.email;
-    const Password=req.body.password;
+exports.createUser = (req, res, next) => {
+    const Name = req.body.name;
+    const Email = req.body.email;
+    const Password = req.body.password;
     Users.findOne({ where: { email: Email } })
-    .then(existingUser => {
-        if (existingUser) {
-             res.status(403).json({ error: 'Email already in use' });
-        } else {
-            Users.create({
-                name: Name,
-                email: Email,
-                password: Password
-            })
-                .then(() => {
-                    res.sendStatus(200);
+        .then(existingUser => {
+            if (existingUser) {
+                console.log('you are in the console of controller 201');
+               return res.status(201).json({ error: 'Email already in use' });
+
+            } else {
+                   Users.create({
+                    name: Name,
+                    email: Email,
+                    password: Password
                 })
-                .catch((err) => {
-                    console.log(err);
-                    res.sendStatus(500);
-                });
-        }
-    })
-    .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-    });
+                    .then(() => {
+                        console.log('email created in controllers 200')
+                       return res.status(200).json({ message: 'Email sent successfully of control 200' });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        console.log('you are in the console of controller 500');
+                       return res.status(500).json({ error: 'controller error' });
+                    });
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.status(500).json({ error: 'controller error' });
+        });
 }
--- frontend/frontend.js
+++ frontend/frontend.js
@@ -3,40 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const Email = document.getElementById('email');
     const Password = document.getElementById('password');
     const SignUp = document.getElementById('signup');
-    const UserList=document.getElementById(('userlist'))
+    const UserList = document.getElementById(('userlist'));
 
-    SignUp.addEventListener('click', () => {
-        const NameValue=Name.value;
-        const EmailValue=Email.value;
-        const PasswordValue=Password.value; 
-        
-        axios.post('http://localhost:3000/user/signup', {
-            name:NameValue,
-            email:EmailValue,
-            password:PasswordValue
-        })
-            .then(response => {
-                console.log('let me check if the console is working');
-                if (response.status === 200) {  
-                    console.log('email added');
-                    const doneMessage = document.createElement('div');
-                    doneMessage.textContent = 'Email is added';
-                    doneMessage.style.color = 'blue';
-                    UserList.appendChild(doneMessage);
-                  
-                }else if(response.status === 403){
-                    console.log('email is already used');
-                  const errorMessage = document.createElement('div');
-                    errorMessage.textContent = 'User already exists';
-                    errorMessage.style.color = 'red';
-                    UserList.appendChild(errorMessage);
-                }
-                
-                
-                else {
-                    console.log('Unexpected status code:', response.status);
-                }
-            })
-            .catch(err => console.error(err));
+    SignUp.addEventListener('click', async (event) => {
+        event.preventDefault(); 
+        const NameValue = Name.value;
+        const EmailValue = Email.value;
+        const PasswordValue = Password.value;
+
+        if (NameValue === '' || EmailValue === '' || PasswordValue === '') {
+            console.log('Please fill in all the fields');
+            return; // Prevent the request if any field is empty
+        }
+        try {
+            const response = await axios.post('http://localhost:3000/user/signup', {
+                name: NameValue,
+                email: EmailValue,
+                password: PasswordValue
+            });
+            if (response.status === 200) {
+                console.log(response.data.message);
+            } else if (response.status === 201) {
+                console.log(response.data.error);
+                const errorMessage = document.createElement('div');
+                errorMessage.textContent = 'User already exists';
+                errorMessage.style.color = 'red';
+                UserList.appendChild(errorMessage);
+            } else {
+                console.log('i am in else');
+                console.log('Unexpected status code:', response.status);
+            }
+        } catch (err) {
+            console.error(err);
+        }
     });
 });
