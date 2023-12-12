78bf379e2ab6794ccfa65ded6c9814dca60f4577
narendramamtora <narendramamtora97@gmail.com>
2023-12-12 17:06:06 +0530

    exp app Production ready

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,5 +1,6 @@
 const Expense = require('../models/expense');
 const User= require('../models/user');
+const sequelize= require('../util/database');
 
 exports.getAllExpenses = async (req, res, next) => {
   try {
@@ -16,6 +17,8 @@ exports.getAllExpenses = async (req, res, next) => {
 };
 
 exports.postAddExpense = async (req, res, next) => {
+  const t= await sequelize.transaction();
+  try { 
   const expAmount = parseFloat(req.body.expense);
   const description = req.body.descrip;
   const select = req.body.mselect;
@@ -24,30 +27,35 @@ exports.postAddExpense = async (req, res, next) => {
 
   console.log('*****',expAmount, description);
   
-  try {
     const newExpense = await Expense.create({
       expamount: expAmount,
       description: description,
       select: select,
-      userId:userId
+      userId:userId,
+    }, {
+      transaction: t,
     });
     const user = await User.findByPk(userId);
 
     if (user) {
       // Update the totalExpense field for the user
       user.totalExpense = (user.totalExpense || 0) + expAmount;
-      await user.save();
+      await user.save({ transaction: t });
     } else {
-      console.error('User not found');
+      console.log('User not found');
     }
-    console.log('Created expense:', newExpense);
+    await t.commit();
     res.status(201).json(newExpense);
   } catch (err) {
+    console.log('i am in the catch 13.03');
+    await t.rollback();
     console.error('Error creating expense:', err);
+    res.status(500).json({ message: 'Error creating expense' });
   }
 
 };
 exports.postDeleteExpense = async (req, res, next) => {
+  const t= await sequelize.transaction();
   const expeId = req.params.expeId; // Use req.params.expeId
   try {
     const expense = await Expense.findByPk(expeId);
@@ -57,20 +65,23 @@ exports.postDeleteExpense = async (req, res, next) => {
 
 
     const expAmount = expense.expamount;
-    const result = await expense.destroy();
+//    const result = await expense.destroy();
     const userId = expense.userId;
     const user = await User.findByPk(userId);
 
     if (user) {
       user.totalExpense = (user.totalExpense || 0) - expAmount;
-      await user.save();
+      await user.save({ transaction: t });
     } else {
+      await t.rollback();
       console.error('User not found');
     }
+    await t.commit();
     console.log('deleted successfully');
     res.status(200).json({ message: 'deleted successfully' });
   } catch (err) {
     console.log(err);
+    await t.rollback();
     res.status(500).json({ message: 'Failed to delete' });
   }
 };
