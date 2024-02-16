6145a16cfd85b679f24327737d1688ad14c2f123
narendramamtora <137789140+narendramamtora@users.noreply.github.com>
2024-02-16 10:08:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path=require('path')
 const express = require('express');
 const helmet = require('helmet');
 const Compression = require('compression');
-const Morgan = require('morgan');
 const bodyParser = require('body-parser');
 const PORT=process.env.PORT;
 
@@ -28,16 +27,10 @@ const ShowLeaderBoeardRoute=require('./routes/premium/showleaderboard');
 const MONGODB_URI =
   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
-  
-const accessLogStream =fs.createWriteStream(
-  path.join(__dirname,'access.log'),
-  {flags:'a'}
-);
 const ReportRoute=require('./routes/report.js');
 app.use(cors());
 app.use(helmet());
 app.use(Compression());
-app.use(Morgan('combined',{stream:accessLogStream}));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
